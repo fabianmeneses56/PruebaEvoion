b65a097183ff3989c2e2e42a6759039dbcbd2340
fabianmeneses <edisonsalamanca30@gmail.com>
2021-01-06 16:51:48 -0500

    INIT

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { Tab } from './App/navigator/Tab';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+    <Tab/>
+    </>
+  );
+};
+
+
+
+export default App;
